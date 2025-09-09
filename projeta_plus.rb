9ddd3f1e9bb17736d370d6f04f71b03ee796488b
Lucas Lauriano de Souza <lucaslauriano.souza@gmail.com>
2025-09-09 19:47:48 -0300

    removing comments from root file

--- projeta_plus.rb
+++ projeta_plus.rb
@@ -1,28 +1,18 @@
 require "sketchup.rb"
 require "extensions.rb"
 
-# Define o módulo principal para o plugin para encapsular todo o código e evitar conflitos.
 module ProjetaPlus
-  # Define a versão do plugin.
   VERSION = "1.0.0".freeze
 
-  # Obtém o caminho para o diretório atual onde este arquivo reside.
-  # Isso é crucial para carregar outros arquivos relativos à raiz do plugin.
   PATH = File.dirname(__FILE__).freeze
 
-  # Define o objeto de extensão para o Gerenciador de Extensões do SketchUp.
-  # O segundo argumento deve apontar para o script principal que o SketchUp carregará
-  # quando a extensão for habilitada. Este será um novo arquivo: 'main.rb'.
   extension = SketchupExtension.new("PROJETA PLUS", File.join(PATH, 'projeta_plus', 'main.rb'))
   extension.description = "Projeta+ - A plugin  library "
   extension.version = VERSION
   extension.copyright = "My Awesome Company © 2025"
   extension.creator = "Lucas" # Seu nome como criador
 
-  # Registra a extensão no SketchUp.
-  # O argumento `true` garante que ela seja visível no Gerenciador de Extensões.
   Sketchup.register_extension(extension, true)
 
-  # Adiciona uma mensagem ao Console Ruby indicando que o plugin foi registrado.
   puts "PROJETA PLUS v#{VERSION} registered and awaiting activation!"
 end
