593267ff26e5fbd6f8cc27198a7d7a3c382d96dc
Lucas Lauriano de Souza <lucaslauriano.souza@gmail.com>
2025-10-28 15:52:53 -0300

    feat: updating projeta_plus.rb file

--- projeta_plus.rb
+++ projeta_plus.rb
@@ -3,16 +3,16 @@ require "extensions.rb"
 
 module ProjetaPlus
   VERSION = "2.0.0".freeze
-
   PATH = File.dirname(__FILE__).freeze
 
   extension = SketchupExtension.new("PROJETA PLUS", File.join(PATH, 'projeta_plus', 'main.rb'))
-  extension.description = "Projeta+ - A plugin  library "
+  extension.description = "Ferramenta completa para anotações arquitetônicas, ambientes, iluminação e gestão de projetos no SketchUp."
   extension.version = VERSION
-  extension.copyright = "My Awesome Company © 2025"
-  extension.creator = "Lucas" # Seu nome como criador
-
+  extension.copyright = "© 2025 Francieli Madeira & Lucas Lauriano"
+  extension.creator = "Francieli Madeira & Lucas Lauriano"
+  
+  # Adicionar metadados adicionais
   Sketchup.register_extension(extension, true)
 
-  puts "PROJETA PLUS v#{VERSION} registered and awaiting activation!"
+  puts "PROJETA PLUS v#{VERSION} carregado com sucesso!"
 end
